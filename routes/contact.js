e505ae90b330f9e028bcc937258a822e89ce58d0
Steven Bister (University of Birmingham Conferences and Events) <s.bister@bham.ac.uk>
2020-04-18 16:31:14 +0100

    Tidied for easier reading

--- routes/contact.js
+++ routes/contact.js
@@ -16,31 +16,40 @@ router.get('/', (req, res) => {
 });
 
 // POST to contact form
-router.post('/', [
+router.post('/', 
+  [
   // Validate form & sanitize the inputs so they're consistent
-    check('name').isLength({min: 1}).withMessage('Name is required').trim(),
-    check('email').isEmail().withMessage('Email needs to be a vaild email address')
+    check('name')
+      .isLength({min: 1})
+      .withMessage('Name is required')
+      .trim(),
+    check('email')
+      .isEmail()
+      .withMessage('Email needs to be a vaild email address')
       .bail() // stops vaildators early if previous ones have failed
       .trim()
       .normalizeEmail(),
-    check('message').isLength({min: 1}).withMessage('Message is required').trim()
-  ], 
+    check('message')
+      .isLength({min: 1})
+      .withMessage('Message is required')
+      .trim()
+  ],
   (req, res) => {
     const errors = validationResult(req);
-    const messagesFilePath = path.join(appRoot.path, 'messages');
     
     res.render('contact', {
       title: 'Contact',
       data: req.body,
       errors: errors.mapped()
     });
-
+    
     // Sanitise data
     const data = matchedData(req);
+    const messagesFilePath = path.join(appRoot.path, 'messages');
     // Check if there are errors and write sanitised to file
     if (errors.errors.length === 0) {
       fs.writeFile(`${messagesFilePath}/${handleDateFormat}.txt`, JSON.stringify(data), (err) => {
-        if(err) console.log(err);
+        if (err) console.log(err);
     
         console.log('Mesage saved!');
       })
