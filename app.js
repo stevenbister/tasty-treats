33193786747b877db7cc2b3d65851bb2116bb428
Steven Bister (University of Birmingham Conferences and Events) <s.bister@bham.ac.uk>
2020-04-18 13:38:47 +0100

    Added parser to handle form submissions

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 
+// Need to include body parser to expose submitted form values on req.body
+const bodyParser = require('body-parser');
+
 const indexRouter = require('./routes/index');
 const contactRouter = require('./routes/contact');
 
@@ -28,6 +31,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/contact', contactRouter);
+
+// Body parser setup
+bodyParser.urlencoded({ extended: true });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
