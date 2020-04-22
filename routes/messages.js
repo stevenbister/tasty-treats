8d992690415d9c02f18516115482780a8ecfc2c6
Steven Bister (University of Birmingham Conferences and Events) <s.bister@bham.ac.uk>
2020-04-22 12:13:09 +0100

    Added call to mongodb to pull messages from db and add to response

--- routes/messages.js
+++ routes/messages.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const messagesJson = require('../messages/messages.json');
-const { messages } = messagesJson;
+// Connect to mongodb
+const { MongoClient } = require('mongodb');
+
 
 // I would make some kind of function to loop over the /routes folder to dynamically create this
 const nav = {
@@ -12,12 +13,19 @@ const nav = {
   'Contact': '/contact'
 }
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+
+  // Connect to mongodb
+  const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true,   useUnifiedTopology: true });
+  const db = client.db('tasty-treats-db');
+
+  const messagesInfo = await db.collection('messages').find({}).toArray();
+// TODO: Display messages most recent first
   res.render('messages', {
     title: 'Messages',
     nav,
-    numberOfMessages: messages.length,
-    messages
+    numberOfMessages: messagesInfo.length,
+    messages: messagesInfo
   })
 
 });
